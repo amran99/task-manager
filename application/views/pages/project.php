ab8ee157538995052d31f7c85ec35f45cb7bb6eb
unknown <u1357440@CW301-22.AD.HUD.AC.UK>
2016-04-05 10:36:45 +0100

    shorten code

--- application/views/pages/project.php
+++ application/views/pages/project.php
@@ -12,12 +12,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>Welcome to the Task Manager</h1>
 	<p><a href="<?php echo base_url()."index.php/home";?>">Back to Projects</a></p>
 	<?php echo "<h2>Project: ".$project[0]->project_name."</h2>";?>
+
+	<?php
 	
-	<div class="column">
-		<h3>Not Started</h3>
-		<?php
+	$h3 = ["Not Started","Started","To Review","Finished"];
+	
+	for($i=0;$i<4;$i++){
+		echo "<div class='column'>";
+		echo "<h3>".$h3[$i]."</h3>";
 		foreach ($projectTasks as $task){
-			if($task->task_status==="not started"){
+			if($task->task_status===strtolower($h3[$i])){
 				echo "<div class='hiddenContent' id='".$task->task_id."'>";
 				echo "<p><span class='hiddenContentTitle' onclick='openContent(".$task->task_id.")'>Task Name: ".$task->task_name."</span></br>";
 				echo "Task Description: ".$task->task_description."</br>";
@@ -30,94 +34,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				echo form_label('', 'task_id');
 				echo "<input type='text' class='hide' name='task_id' value='". $task->task_id ."'>";
 				echo form_label('Move To:', 'movetaskto');
-				echo "<select name='movetaskto' required><option value='started'>Started</option><option value='to review'>To Review</option><option value='finished'>Finished</option></select>";
+				echo "<select name='movetaskto' required>";
+				echo "<option value='not started'>Not Started</option>";
+				echo "<option value='started'>Started</option>";
+				echo "<option value='to review'>To Review</option>";
+				echo "<option value='finished'>Finished</option>";
+				echo "</select>";
 				echo form_submit('add_btn', 'Update Task');
 				echo form_close();
 				echo "</div>";
 			}
 		}
-		?>
-	</div>
-	
-	<div class="column">
-		<h3>Started</h3>
-		<?php
-		foreach ($projectTasks as $task){
-			if($task->task_status==="started"){
-				echo "<div class='hiddenContent' id='".$task->task_id."'>";
-				echo "<p><span class='hiddenContentTitle' onclick='openContent(".$task->task_id.")'>Task Name: ".$task->task_name."</span></br>";
-				echo "Task Description: ".$task->task_description."</br>";
-				echo "Task Start: ".$task->task_start."</br>";
-				echo "Task Finish: ".$task->task_finish."</br>";
-				echo "Task Status: ".$task->task_status."</p>";
-				echo form_open('home/updatetask');
-				echo form_label('', 'project_id');
-				echo "<input type='text' class='hide' name='project_id' value='". $project[0]->project_id ."'>";
-				echo form_label('', 'task_id');
-				echo "<input type='text' class='hide' name='task_id' value='". $task->task_id ."'>";
-				echo form_label('Move To:', 'movetaskto');
-				echo "<select name='movetaskto' required><option value='not started'>Not Started</option><option value='to review'>To Review</option><option value='finished'>Finished</option></select>";
-				echo form_submit('add_btn', 'Update Task');
-				echo form_close();
-				echo "</div>";
-			}
-			
-		}
-		?>
-	</div>
-	
-	<div class="column">
-		<h3>To Review</h3>
-		<?php
-		foreach ($projectTasks as $task){
-			if($task->task_status==="to review"){
-				echo "<div class='hiddenContent' id='".$task->task_id."'>";
-				echo "<p><span class='hiddenContentTitle' onclick='openContent(".$task->task_id.")'>Task Name: ".$task->task_name."</span></br>";
-				echo "Task Description: ".$task->task_description."</br>";
-				echo "Task Start: ".$task->task_start."</br>";
-				echo "Task Finish: ".$task->task_finish."</br>";
-				echo "Task Status: ".$task->task_status."</p>";
-				echo form_open('home/updatetask');
-				echo form_label('', 'project_id');
-				echo "<input type='text' class='hide' name='project_id' value='". $project[0]->project_id ."'>";
-				echo form_label('', 'task_id');
-				echo "<input type='text' class='hide' name='task_id' value='". $task->task_id ."'>";
-				echo form_label('Move To:', 'movetaskto');
-				echo "<select name='movetaskto' required><option value='not started'>Not Started</option><option value='started'>Started</option><option value='finished'>Finished</option></select>";
-				echo form_submit('add_btn', 'Update Task');
-				echo form_close();
-				echo "</div>";
-			}
-			
-		}
-		?>
-	</div>
-	
-	<div class="column">
-		<h3>Finished</h3>
-		<?php
-		foreach ($projectTasks as $task){
-			if($task->task_status==="finished"){
-				echo "<div class='hiddenContent' id='".$task->task_id."'>";
-				echo "<p><span class='hiddenContentTitle' onclick='openContent(".$task->task_id.")'>Task Name: ".$task->task_name."</span></br>";
-				echo "Task Description: ".$task->task_description."</br>";
-				echo "Task Start: ".$task->task_start."</br>";
-				echo "Task Finish: ".$task->task_finish."</br>";
-				echo "Task Status: ".$task->task_status."</p>";
-				echo form_open('home/updatetask');
-				echo form_label('', 'project_id');
-				echo "<input type='text' class='hide' name='project_id' value='". $project[0]->project_id ."'>";
-				echo form_label('', 'task_id');
-				echo "<input type='text' class='hide' name='task_id' value='". $task->task_id ."'>";
-				echo form_label('Move To:', 'movetaskto');
-				echo "<select name='movetaskto' required><option value='not started'>Not Started</option><option value='started'>Started</option><option value='to review'>To Review</option></select>";
-				echo form_submit('add_btn', 'Update Task');
-				echo form_close();
-				echo "</div>";
-			}
-		}
-		?>
-	</div>
+		echo "</div>";
+	}
+	?>
 	
 	<?php
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////
--- javascript/jquery.js
+++ javascript/jquery.js
@@ -1,18 +1,3 @@
-//tabs for login
-if (document.getElementById("signup")) {
-	jQuery(document).ready(function() {
-		jQuery('.tabs .tab-links a').on('click', function(e)  {
-			var currentAttrValue = jQuery(this).attr('href');
-			// Show/Hide Tabs
-			jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
-			// Change/remove current tab to active
-			jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
-			e.preventDefault();
-		});
-	});
-}
-
-
 function openContent(task_id){
 	var content = document.getElementById(task_id);
 	if (content.style.height=="160px") {
