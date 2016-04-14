df029ba9660c0f58e553f421c53fa2491186e638
unknown <u1357440@CW204-09.AD.HUD.AC.UK>
2016-04-14 13:46:14 +0100

    registeration fix

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -6,21 +6,24 @@ class Users extends CI_Controller
   $email=$this->input->post('email');
   $this->load->model("UsersModel");
   $users=$this->UsersModel->getUsers();
+  $success = true;
   foreach($users as $user){
    if($user->email == $email){
-    $first_name=$this->input->post('first_name');
-    $last_name=$this->input->post('last_name');
-    $email=$this->input->post('email');
-    $password=$this->input->post('password');
-    $this->load->model("UsersModel");
-    if($this->UsersModel->insertUser($first_name, $last_name, $email, $password)){
+    $success = false;
+    $data['msg']="The email you used is already regitered, please try again with a different email or login with your old one.";
+   }
+  }
+  if($success = true){
+   $first_name=$this->input->post('first_name');
+   $last_name=$this->input->post('last_name');
+   $email=$this->input->post('email');
+   $password=$this->input->post('password');
+   $this->load->model("UsersModel");
+   if($this->UsersModel->insertUser($first_name, $last_name, $email, $password)){
      $data['msg']="Your Registeration has been successful, please login";
     }else{
       $data['msg']="Something went wrong, please try again";
     }
-   }else{
-    $data['msg']="The email you used is already regitered, please try again with a different email or login with your old one.";
-   }
   }
  $this->load->view('pages/login_view', $data);
  }
